b2c995226e81716d3e4885ecde937c08f6ee0632
Nasir Nooruddin <nasir@nasir.id>
2021-10-19 19:33:29 +0700

    revision 2

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://www.detik.com/terpopuler", params={"tag_from" : "wp_cb_mostPopular_more"})
+soup = BeautifulSoup(response.text, "html.parser")
+popular_area = soup.find(attrs={"class":"grid-row list-content"})
+titles = popular_area.findAll(attrs={"class":"media__title"})
+images = popular_area.findAll(attrs={"class":"media__image"})
+
+for image in images:
+    print(image.find("a").find("img")["title"])
